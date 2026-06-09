015c4b37b8eab1bb416432cf8401c8ad5fe1a6d9
jitisha-stack <jitishasrivastava@gmail.com>
2026-06-09 10:36:20 +0530

    Track local product of digit

--- Day-2/q7_product_of_digit.cpp
+++ Day-2/q7_product_of_digit.cpp
@@ -1,13 +1,13 @@
-#include<iostream>
-int main(){
-    int n , d , product=1 ;
-    std::cout<<"Enter A Number : ";
-    std::cin>>n;
-    while(n>0){
-        d = n% 10; //last digit of number
-        product *= d ; //multiply the digit to product
-        n /=10 ; //remove last digit 
-    }
-    std::cout<<"Product Of Digit Of Number is : "<<product;
-    return 0;
-}
+// #include<iostream>
+// int main(){
+//     int n , d , product=1 ;
+//     std::cout<<"Enter A Number : ";
+//     std::cin>>n;
+//     while(n>0){
+//         d = n% 10; //last digit of number
+//         product *= d ; //multiply the digit to product
+//         n /=10 ; //remove last digit 
+//     }
+//     std::cout<<"Product Of Digit Of Number is : "<<product;
+//     return 0;
+// }
